36b2aeda150fdc73ad90e596b1ead17063399c14
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-25 16:46:16 +0700

    Add switch case 1

--- Exercise3_150/Program.cs
+++ Exercise3_150/Program.cs
@@ -80,6 +80,14 @@ namespace Exercise_Linked_List_A
                     Console.WriteLine("4. Exit");
                     Console.Write("\nEnter your choice (1-4): ");
                     char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                    }
                 }
             }
         }
