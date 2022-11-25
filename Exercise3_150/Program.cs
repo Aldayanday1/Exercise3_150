244ebf34fe6043fc0b42c757da741c082b0f9a90
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-25 16:52:39 +0700

    Add switch case 2

--- Exercise3_150/Program.cs
+++ Exercise3_150/Program.cs
@@ -87,6 +87,29 @@ namespace Exercise_Linked_List_A
                                 obj.traverse();
                             }
                             break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("List is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("Record found");
+                                    Console.WriteLine("Roll number: " +
+                                        curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
+
                     }
                 }
             }
