0f09ff2a04baa61f0a3d391209df9bc28fb6b2c6
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-25 16:53:04 +0700

    Add switch case 3

--- Exercise3_150/Program.cs
+++ Exercise3_150/Program.cs
@@ -109,6 +109,11 @@ namespace Exercise_Linked_List_A
                                 }
                             }
                             break;
+                        case '3':
+                            {
+                                obj.firstNode();
+                            }
+                            break;
 
                     }
                 }
